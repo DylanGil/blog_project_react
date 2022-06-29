975d39712a01b567cd50ad3b211adfb29c2a6efc
Dylan GIL AMARO <dylan.flame.libra@gmail.com>
2022-06-29 16:00:53 +0200

    just a test to delete

--- front/pages/404.jsx
+++ front/pages/404.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
       <div className="flex items-center justify-center h-screen">
         <p className="flex items-center justify-center">
           <span className="flex items-center font-bold text-3xl border-r-2 border-gray-400 pr-5 mr-5 h-16">
-            404
+            405
           </span>
           <span className="text-2xl flex items-center">YA PAS WESH !!</span>
           <span className="text-8xl flex items-center">💻😭</span>
